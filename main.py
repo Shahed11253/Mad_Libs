f12ec5f08d1a6d02a5abbf0cb8a7f5757635428a
Shahed11253 <s06247605@gmail.com>
2025-06-03 22:44:20 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from threading import main_thread
+
+username = input("what is your name:")
+fathername = input("what is your father name:")
+mothername = input("what is your mother name:")
+study = input("which class do you read in:")
+relationship = input("what is your relationship status:")
+
+print(f'my Doggy Name is {username} , My doggys father name {fathername} , my doggys mother name is {mothername} '
+      f'my doggy study in class{study} , my doggys relationship status{relationship} ')
